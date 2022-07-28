eda4febc273a12c425f6e2d094f5ea28a88a22e4
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-07-28 13:16:14 +0300

    added Interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
+import { ITask } from './Interface';
 
 const App = () => {
 
 const [task, setTask] = useState<string>('');
-const [todoList, setTodoList] = useState([]);
+const [todoList, setTodoList] = useState<ITask[]>([]);
 
   return (
     <div>
