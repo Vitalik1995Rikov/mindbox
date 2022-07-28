b361bdeb61347a939799602e305e7c42ea4a8c7e
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-07-28 13:13:19 +0300

    Added some first tags and two useStates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+
+const [task, setTask] = useState<string>('');
+const [todoList, setTodoList] = useState([]);
+
   return (
     <div>
-      Start
+      <div>Todos</div>
+      <div className='flex'>
+        <input type="text" placeholder='What needs to be done?'/>
+        <button>Add</button>
+      </div>
     </div>
   );
 };
