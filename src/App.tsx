16c66a7bfe05767bb69d435827bdb48ef0a086c8
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-07-28 13:19:35 +0300

    added handleChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, ChangeEvent} from 'react';
 import { ITask } from './Interface';
 
 const App = () => {
@@ -6,11 +6,16 @@ const App = () => {
 const [task, setTask] = useState<string>('');
 const [todoList, setTodoList] = useState<ITask[]>([]);
 
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  setTask(event.target.value)
+  console.log(task)
+}
   return (
     <div>
       <div>Todos</div>
       <div className='flex'>
-        <input type="text" placeholder='What needs to be done?'/>
+        <input type="text" placeholder='What needs to be done?' onChange={handleChange}/>
         <button>Add</button>
       </div>
     </div>
